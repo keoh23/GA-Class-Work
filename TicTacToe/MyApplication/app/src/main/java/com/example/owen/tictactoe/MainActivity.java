76a3458c91e721985725f5ae2fb7e8c921db15da
keoh23 <kevin.oh23@gmail.com>
2016-08-15 12:10:52 -0700

    initial push of tictactoe game
    alpha version of the layout

---
+++ TicTacToe/MyApplication/app/src/main/java/com/example/owen/tictactoe/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.owen.tictactoe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean ttTurn = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
